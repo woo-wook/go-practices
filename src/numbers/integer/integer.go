596e9e53838e477a304c2a29400d9e1c05c086b4
Hanwook Ryu <ryu@hanwook.me>
2023-02-07 20:02:21 +0900

    Add: 정수

---
+++ src/numbers/integer/integer.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 int = 32
+	var num2 int = -15
+	var num3 int = 0723
+	var num4 int = 0x32fa2c75
+
+	fmt.Printf("%d, %d, %d, %d", num1, num2, num3, num4)
+}
