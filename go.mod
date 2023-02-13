3ffd4844a32815ee43ebbb88a3674010d154ff66
Hanwook Ryu <ryu@hanwook.me>
2023-02-13 18:00:53 +0900

    Add: 인터넷 소스 저장소의 패키지 사용하기

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module go-practices
+
+go 1.20
+
+require github.com/agrison/go-commons-lang v0.0.0-20200208220349-58e9fcb95174 // indirect
---
+++ src/onlinepackage/onlinepackage.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/agrison/go-commons-lang/stringUtils"
+)
+
+func main() {
+	s := ""
+	fmt.Println(stringUtils.IsEmpty(s))
+}
